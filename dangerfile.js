ef432a0e0751210c8d7552232a295e62fac452a0
sumatrabean <kennyho.code@gmail.com>
2022-06-11 23:39:28 -0400

    add packages

--- dangerfile.js
+++ dangerfile.js
@@ -1,13 +1,33 @@
-import { fail, warn, message, markdown, danger } from "danger"
+import { fail, warn, message, markdown, danger } from 'danger';
 
-warn("This is a warning")
-message("This is a normal message")
-markdown("*Markdown* is also **supported**")
+warn('This is a warning');
+message('This is a normal message');
+markdown('*Markdown* is also **supported**');
 
-const { additions = 0, deletions = 0 } = danger.github.pr
-message(`:tada: The PR added ${additions} and removed ${deletions} lines.`)
+const { additions = 0, deletions = 0 } = danger.github.pr;
+message(`:tada: The PR added ${additions} and removed ${deletions} lines.`);
 
-const tests = danger.git.fileMatch("*/__tests__/*")
+const tests = danger.git.fileMatch('*/__tests__/*');
 if (!tests.modified) {
-  warn("You have app changes without tests.")
+  warn('You have app changes without tests.');
+}
+
+import commitlint from 'danger-plugin-conventional-commitlint';
+import configConventional from '@commitlint/config-conventional';
+
+(async function dangerReport() {
+  const commitlintConfig = {
+    severity: 'warn',
+  };
+  await commitlint(configConventional.rules, commitlintConfig);
+})();
+
+// Warns if there are changes to package.json, and tags the team.
+const packageChanged = includes(danger.git.modified_files, 'package.json');
+if (packageChanged) {
+  const title = ':lock: package.json';
+  const idea =
+    'Changes were made to package.json. ' +
+    'This will require a manual import by a Facebook employee.';
+  warn(`${title} - <i>${idea}</i>`);
 }

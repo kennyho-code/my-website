68045347b0f7b641719d6928249844aed8e87938
Kenny Ho <kennyho.code@gmail.com>
2022-06-11 22:56:34 -0400

    Update dangerfile.js

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,9 @@
-import {message, danger} from "danger"
+import { fail, warn, message, markdown, danger } from "danger"
 
-const modifiedMD = danger.git.modified_files.join("- ")
-message("Changed Files in this PR: \n - " + modifiedMD)
+fail("This is a failure message")
+warn("This is a warning")
+message("This is a normal message")
+markdown("*Markdown* is also **supported**")
+
+const { additions = 0, deletions = 0 } = danger.github.pr
+message(`:tada: The PR added ${additions} and removed ${deletions} lines.`)
